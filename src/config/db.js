e060e27a4528e98d2d2511c8aca6af269ab9da62
alvareztran <tvbang176@gmail.com>
2026-05-21 23:14:57 +0700

    show err

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const connectDb = async () => {
         await mongoose.connect(process.env.DATABASE_URI);
         console.log("Database connection successfully.");
     } catch (err) {
-        console.log("Database connection failed.");
+        console.log("Database connection failed: ", err);
         process.exit(1);
     }
 }
